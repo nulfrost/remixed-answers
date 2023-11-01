04c570d865de8bc5c0afd2e3d6511b8869bcc9f9
nulfrost <khadane.miller@gmail.com>
2023-10-31 23:03:15 -0400

    schema

---
+++ app/drizzle/schema.server.ts
@@ -0,0 +1,73 @@
+import {
+  pgTable,
+  varchar,
+  text,
+  serial,
+  timestamp,
+  integer,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { createId } from "@paralleldrive/cuid2";
+
+export const users = pgTable("users", {
+  id: varchar("id", { length: 24 })
+    .$defaultFn(() => createId())
+    .primaryKey()
+    .notNull(),
+  username: text("username"),
+  email: text("email"),
+  created_at: timestamp("created_at").defaultNow(),
+});
+
+export const questions = pgTable("questions", {
+  id: serial("id").notNull().primaryKey(),
+  title: text("title"),
+  body: text("body"),
+  created_at: timestamp("created_at").defaultNow(),
+  updated_at: timestamp("updated_at").defaultNow(),
+  userId: text("user_id").notNull(),
+  categoryId: integer("category_id").notNull(),
+});
+
+export const answers = pgTable("answers", {
+  id: serial("id").notNull().primaryKey(),
+  content: text("content"),
+  created_at: timestamp("created_at").defaultNow(),
+  updated_at: timestamp("updated_at").defaultNow(),
+  questionId: integer("question_id").notNull(),
+  userId: text("user_id").notNull(),
+});
+
+export const categories = pgTable("categories", {
+  id: serial("id").notNull().primaryKey(),
+  name: text("name"),
+});
+
+// relations
+export const questionsRelations = relations(questions, ({ many, one }) => ({
+  answers: many(answers),
+  category: one(categories, {
+    fields: [questions.categoryId],
+    references: [categories.id],
+  }),
+  author: one(users, {
+    fields: [questions.userId],
+    references: [users.id],
+  }),
+}));
+
+export const answersRelations = relations(answers, ({ one }) => ({
+  question: one(questions, {
+    fields: [answers.questionId],
+    references: [questions.id],
+  }),
+}));
+
+export const usersRelations = relations(users, ({ many }) => ({
+  questions: many(questions),
+  answers: many(answers),
+}));
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  questions: many(questions),
+}));
