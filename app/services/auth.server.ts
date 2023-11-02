0e6a477383113e3e1c1a36ab489c01e4bd592731
nulfrost <khadane.miller@gmail.com>
2023-11-01 23:23:59 -0400

    add remix-auth

---
+++ app/services/auth.server.ts
@@ -0,0 +1,12 @@
+import { Authenticator } from "remix-auth";
+import { FormStrategy } from "remix-auth-form";
+import { sessionStorage } from "~/services/session.server";
+
+export let authenticator = new Authenticator(sessionStorage);
+
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    const username = form.get("username");
+    const password = form.get("password");
+  })
+);
