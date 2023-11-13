f9236f49a1c564db8b4f48ff489b2e48727e1f97
nulfrost <khadane.miller@gmail.com>
2023-11-12 23:37:22 -0500

    ci: is this a dotenv thing

--- app/drizzle/config.server.ts
+++ app/drizzle/config.server.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema.server";
+import "dotenv/config";
 
 const queryClient = postgres(process.env.DATABASE_URL as string);
 export const db = drizzle(queryClient, { schema });
